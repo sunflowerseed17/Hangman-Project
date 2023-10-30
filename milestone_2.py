bdea739befbf6caf983b068e5368bb3e2a391273
Natasza Siwinska <natsiwinska@gmail.com>
2023-10-30 17:46:29 +0000

    update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,19 @@
 import random
 
-word_list = strawberries, mangoes, blueberries, grapefruit, oranges
+#This is the first task
+
+word_list = "strawberries", "mangoes", "blueberries", "grapefruit", "oranges"
 print(word_list)
 
 word = random.choice(word_list)
 
 print(word)
+
+#This is the second task
+
+guess = input("Please enter a single letter ")
+
+print(type(guess))
+
+if len(guess) == 1:
+    print("Goess guess! ")
